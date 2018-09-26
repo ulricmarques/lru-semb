46923c49bd7aaba2b5480b75c2b766db052c0093
Ulric Marques <ulricmf@gmail.com>
2018-09-26 14:19:25 -0300

    Adicionado arquivo de texto

--- lru.c
+++ lru.c
@@ -1,53 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int num_referencias,num_frames;
+int num_referencias, num_frames;
 int referencias[3000];
-int paginas[10];
-int acerto=0;
-int i,j,k;
-int cont_faltas=0;
+int frames[50];
+int acerto = 0;
+int i, j, k;
+int cont_faltas = 0;
 
-void gera_referencias(){
-	num_frames = 4;
-	num_referencias = 30;
-	time_t t;
-	srand((unsigned) time(&t));
-	int i;
-	for(i=0; i<num_referencias; i++){
-		referencias[i] = rand() % 10;
-	}	
+void le_referencias(){
+    FILE *file;
+    file = fopen("referencias.txt","r");
+    if(file == NULL){ /* Arquivo não encontrado */
+            printf("\n\n[!] Arquivo nao localizado");
+            return;
+    }
+    else{
+        int i = 0;
+        while(!feof(file)){
+            fscanf(file, "%d", &referencias[i]);
+            i++;
+
+        }
+        fclose(file);
+    }
 }
 
 void mostra_referencias(){
 	printf("Referencias: ");
-    for (k=0; k<num_referencias; k++){
-    	printf("%d ",referencias[k]);
+    for (k = 0; k < num_referencias; k++){
+    	printf("%d ", referencias[k]);
     }
     printf("\n");
 }
  
 void recebe_dados(){
     printf("\nDigite a quantidade de referencias:");
-    scanf("%d",&num_referencias);
+    scanf("%d", &num_referencias);
     printf("\nDigite a sequencia de referencias:");
-    for(i=0; i<num_referencias; i++)
-        scanf("%d",&referencias[i]);
+    for(i = 0; i < num_referencias; i++)
+        scanf("%d", &referencias[i]);
     printf("\nDigite a quantidade de frames:");
-    scanf("%d",&num_frames);
+    scanf("%d", &num_frames);
 }
- 
+
 void inicializa(){
-    cont_faltas=0;
-    for(i=0; i<num_frames; i++)
-        paginas[i]=9999;
+    cont_faltas = 0;
+    for(i = 0; i < num_frames; i++)
+        frames[i] = 9999;
 }
  
 int foi_acerto(int referencia){
-    acerto=0;
-    for(j=0; j<num_frames; j++){
-        if(paginas[j]==referencia){
-            acerto=1;
+    acerto = 0;
+    for(j = 0; j < num_frames; j++){
+        if(frames[j] == referencia){
+            acerto = 1;
             break;
         }
     }
@@ -56,11 +63,11 @@ int foi_acerto(int referencia){
  
 int pega_indice_acerto(int referencia){
     int indice_acerto;
-    for(k=0; k<num_frames; k++)
+    for(k = 0; k < num_frames; k++)
     {
-        if(paginas[k]==referencia)
+        if(frames[k] == referencia)
         {
-            indice_acerto=k;
+            indice_acerto = k;
             break;
         }
     }
@@ -68,9 +75,9 @@ int pega_indice_acerto(int referencia){
 }
  
 void mostra_paginas(){
-    for (k=0; k<num_frames; k++){
-        if(paginas[k]!=9999)
-            printf(" %d",paginas[k]);
+    for (k =0 ; k < num_frames; k++){
+        if(frames[k] != 9999)
+            printf(" %d", frames[k]);
     }
 }
  
@@ -78,42 +85,42 @@ void mostra_faltas(){
     printf("\nQuantidade de faltas: %d\n", cont_faltas);
 }
  
-void lru(){
+void least_recently_used(){
     inicializa();
-    int menos_recentemente_usados[50];
-    for(i=0; i<num_referencias; i++){
-        printf("\nPara %d :",referencias[i]);
-        if(foi_acerto(referencias[i])==0){
-            for(j=0; j<num_frames; j++){
-                int pagina_atual=paginas[j];
-                int achou=0;
-                for(k=i-1; k>=0; k--){
-                    if(pagina_atual==referencias[k]){
-                        menos_recentemente_usados[j]=k;
-                        achou=1;
+    int lru[50];
+    for(i = 0; i < num_referencias; i++){
+        //printf("\nPara %d :",referencias[i]);
+        if(foi_acerto(referencias[i]) == 0){
+            for(j = 0; j < num_frames; j++){
+                int frame_atual = frames[j];
+                int achou = 0;
+                for(k = i-1; k >= 0; k--){
+                    if(frame_atual == referencias[k]){
+                        lru[j] = k;
+                        achou = 1;
                         break;
                     }
                     else
-                        achou=0;
+                        achou = 0;
                 }
                 if(!achou)
-                    menos_recentemente_usados[j]=-9999;
+                    lru[j] = -9999;
             }
-            int menor=9999;
+            int menor = 9999;
             int indice_substituido;
-            for(j=0; j<num_frames; j++){
-                if(menos_recentemente_usados[j]<menor){
-                    menor=menos_recentemente_usados[j];
-                    indice_substituido=j;
+            for(j = 0; j < num_frames; j++){
+                if(lru[j] < menor){
+                    menor = lru[j];
+                    indice_substituido = j;
                 }
             }
-            paginas[indice_substituido]=referencias[i];
+            frames[indice_substituido] = referencias[i];
             cont_faltas++;
  
-            mostra_paginas();
+            //mostra_paginas();
         }
         else{
-            printf("Acerto!");
+            //printf("Acerto!");
         }
     }
     mostra_faltas();
@@ -121,8 +128,10 @@ void lru(){
 
 int main(){
     //recebe_dados();
-    gera_referencias();
+    num_referencias = 3000;
+    num_frames = 4;
+    le_referencias();
     mostra_referencias();
     
-	lru();
+	least_recently_used();
 }

9ddb1b2b2a676e044ea3283c4b800f6272255a93
Ulric Marques <ulricmf@gmail.com>
2018-11-07 18:09:58 -0200

    Update lru-pic.c

--- lru-pic.c
+++ lru-pic.c
@@ -47,136 +47,88 @@
 #include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define _XTAL_FREQ 32000000
 #define F_CPU 32000000/64//#define Baud_value(baud_rate) (((float)(F_CPU)/(float)baud_rate)-1)
 #define Baud_value (((float)(F_CPU)/(float)baud_rate)-1)//calculo do taxa de transmissão serial para Uart
 
-void inicializa(int frames[], int num_frames){
-    int i; //Vari�vel contadora.
-    for(i = 0; i < num_frames; i++){
-        /* Inicializa o contador "i" com o valor 0, e enquanto ele for menor
-           que "num_frames", atribui a� posi��o "i" do vetor "frames" o valor
-           9999, incrementando o contador em seguida. */
-        frames[i] = 9999;
-    }
+int num_referencias,num_frames;
+char referencias[900];
+char frames[4];
+int acerto=0;
+int i,j,k;
+int cont_faltas=0;
+
+void gera_referencias(){
+	num_frames = 4;
+	num_referencias = 900;
+	int i;
+	for(i=0; i<num_referencias; i++){
+		referencias[i] = i;
+	}	
+}
+
+ 
+void inicializa(){
+    cont_faltas=0;
+    for(i=0; i<num_frames; i++)
+        frames[i]=9999;
 }
  
-
-/*
-    Fun��o:  Verifica se uma determinada p�gina est� alocada em algum 
-             frame. Em caso positivo, ocorreu um acerto. J� em caso
-             negativo, houve uma falta de p�gina.
-    Entrada: Um inteiro representando a p�gina referenciada, um vetor
-             representando os frames e a quantidade de frames existentes.
-    Sa�da:   Retorna valor 0 em caso de falta de p�gina ou valor 1 em caso de
-             acerto.
-*/
-int foi_acerto(int referencia, int frames[], int num_frames){
-    int i; //Vari�vel contadora.
-    int acerto = 0; /*Vari�vel que representa se houve acerto ou falta,
-                      inicializada com 0 (falta). */
-    for(i = 0; i < num_frames; i++){
-        /* Inicializa o contador "i" com o valor 0, e enquanto ele for menor
-           que "num_frames", checa se a posi��o "i" do vetor "frames" cont�m
-           o valor "referencia". Caso contenha, atribui valor 1 � vari�vel
-           contadora e sai do la�o. Incrementando o contador ao final. */
-        if(frames[i] == referencia){
-            acerto = 1;
+int foi_acerto(int referencia){
+    acerto=0;
+    for(j=0; j<num_frames; j++){
+        if(frames[j]==referencia){
+            acerto=1;
             break;
         }
     }
-    return acerto; //Retorna o valor da vari�vel "acerto".
+    return acerto;
 }
-
-/*
-    Fun��o:  Exibe a quantidade de faltas de p�gina ocorridas ap�s a execu��o
-             do algoritmo.
-    Entrada: Um inteiro representando a quantidade de faltas ocorridas.
-    Sa�da:   Exibe no terminal a quantidade de faltas.
-*/
-void mostra_faltas(int cont_faltas){
-    /* Exibe no terminal o valor da vari�vel "cont_faltas". */
-}
+ 
 
 
-/*
-    Fun��o:  Simula a substitui��o de p�ginas de mem�ria utilizando o algoritmo
-             Least Recently Used.
-    Entrada: Um vetor representando os frames, a quantidade de frames existentes,
-             a quantidade de refer�ncias que ser�o analisadas e  um vetor contendo
-             as refer�ncias (onde cada p�gina � representada por um inteiro).
-    Sa�da:   Retorna a quantidade de faltas de p�gina ocorridas.
-*/
-int lru(int frames[], int num_frames, int num_referencias, int referencias[]){
-    int i,j,k; /* Vari�veis contadoras. */
-    short int ultimo_acesso[4]; /* Declara��o do vetor de frames, de tamanho m�ximo 50,
-                              que guardar� o �ltimo �ndice... */
-    int cont_faltas = 0;   /* Contador de faltas de p�gina. */
-    inicializa(frames, num_frames); /* Chamada da fun��o para atribuir aos frames
-                                       existentes o valor 9999, simbolizando o
-                                       estado inicial da mem�ria. */
-
-    for(i = 0; i < num_referencias; i++){
-        /* Inicializa o contador "i" com o valor 0, e enquanto ele for menor
-           que "num_frames", checa se o valor contido na posi��o "i" do vetor
-           "referencias" est� contido no vetor "frames". Caso n�o esteja,
-           executa o c�digo contido no if abaixo. Incrementa o contador ao final. */
-        if(!foi_acerto(referencias[i], frames, num_frames)){
-            for(j = 0; j < num_frames; j++){
-                int frame_atual = frames[j];/* Declara uma vari�vel auxiliar que guarda o
-											valor contido numa posi��o do vetor de frames */
-                int achou = 0; 				/* Vari�vel que representa se a p�gina foi encontrada no vetor de refer�ncias,
-                      						inicializada com 0 (n�o encontrada). */
-                for(k = i-1; k >= 0; k--){  /* Inicializa o contador "k" com o valor da refer�ncia anterior a atual, e enquanto ele for maior
-           									ou igual a 0, checa se a posi��o "k" do vetor "referencias" cont�m
-           									o valor "frame_atual". Caso contenha, atribui valor "k" � posi��o "j" do vetor
-											"ultimo_acesso" e valor 1 a vari�vel "achou", saindo do la�o em seguida.
-											Decrementando o contador ao final. */
-                    if(frame_atual == referencias[k]){
-                        ultimo_acesso[j] = k;
-                        achou = 1;
+ 
+int lru(){
+    inicializa();
+    int menos_recentemente_usados[4];
+    for(i=0; i<num_referencias; i++){
+        //printf("\nPara %d :",referencias[i]);
+        if(foi_acerto(referencias[i])==0){
+            for(j=0; j<num_frames; j++){
+                int pagina_atual=frames[j];
+                int achou=0;
+                for(k=i-1; k>=0; k--){
+                    if(pagina_atual==referencias[k]){
+                        menos_recentemente_usados[j]=k;
+                        achou=1;
                         break;
                     }
-            	}
-                if(!achou)  /* Caso a vari�vel "achou" tenha valor 0, atribui o
-							valor -9999 na posi��o "j" do vetor "ultimo_acesso".*/
-                    ultimo_acesso[j] = -9999;
+                    else
+                        achou=0;
+                }
+                if(!achou)
+                    menos_recentemente_usados[j]=-9999;
             }
-
-            int menor = 9999;	/* Declara uma vari�vel que guardar� o menor �ndice do vetor de "referencias"
-								que contenha a p�gina menos recente do vetor de frames*/
-            int indice_substituido; /* Declara uma vari�vel que guardar� o �ndice da p�gina
-									menos recentemente usada, ou seja, aquela que ser� substitu�da*/
-            for(j = 0; j < num_frames; j++){/* Inicializa o contador "j" com o valor 0, e enquanto ele for menor
-           									que "num_frames", checa se a posi��o "j" do vetor "ultimo_acesso" cont�m
-           									um valor menor que o da vari�vel "menor". Caso contenha, o valor da vari�vel "menor"
-											se torna o valor contido na posi��o "j" do vetor "ultimo_acesso" e a vari�vel
-											"indice_substituido" recebe o valor "j".
-											Incrementando o contador ao final. */
-                if(ultimo_acesso[j] < menor){
-                    menor = ultimo_acesso[j];
-                    indice_substituido = j;
+            int menor=9999;
+            int indice_substituido;
+            for(j=0; j<num_frames; j++){
+                if(menos_recentemente_usados[j]<menor){
+                    menor=menos_recentemente_usados[j];
+                    indice_substituido=j;
                 }
             }
-
-            frames[indice_substituido] = referencias[i];/* Substitui o a p�gina contida no vetor frame na posi��o "indice_substituido"
-														pela refer�ncia atual.*/
-            cont_faltas++; /*Incremanta contador de faltas. */
+            frames[indice_substituido]=referencias[i];
+            cont_faltas++;
+ 
+            //mostra_paginas();
+        }
+        else{
+            //printf("Acerto!");
         }
     }
-    return cont_faltas; /* Retorna a quantidade de faltas de p�gina ocorridas */
+    return cont_faltas;
 }
-
-/*
-    Fun��o:  Ponto de entrada. Declara e inicializa as vari�veis necess�rias,
-             executando o algoritmo uma �nica vez. 
-    Entrada: Nada.
-    Sa�da:   Ao final da execu��o, a quantidade de faltas de p�gina ocorridas
-             � exibida no terminal.
-*/
-
 
 int main(void) {
         TRISA = 0x0F;
@@ -188,15 +140,10 @@ int main(void) {
 //    while (1);
     
     while (1) {
-        
-        //int referencias[] = {1,2,3,4,5,6,7,8,9,7}; //Declar��o do vetor de refer�ncias, de tamanho m�ximo 3000.
-        int referencias[40];
-        int frames[4];        //Declara��o do vetor de frames, de tamanho m�ximo 50.
-        int cont_faltas;       //Vari�vel que guardar� a quantidade de faltas de p�gina.
-
        
-        cont_faltas = lru(frames, 4, 10, referencias); /* Chamada da fun��o de simula��o, 
-                                                        armazenando seu resultado na vari�vel
+        gera_referencias();
+        cont_faltas = lru(); /* Chamada da fun??o de simula??o, 
+                                                        armazenando seu resultado na vari?vel
                                                         "cont_faltas". */
 
       //LATA = 0x90;
@@ -204,7 +151,13 @@ int main(void) {
        LATAbits.LATA5 = 1;
        LATAbits.LATA6 = 1;            
        LATAbits.LATA7 = 1;
-    __delay_ms(500);  
+       __delay_ms(500);
+       LATAbits.LATA4 = 1;
+       LATAbits.LATA5 = 0;
+       LATAbits.LATA6 = 0;            
+       LATAbits.LATA7 = 1;
+       __delay_ms(500);
+    
       
     }
     

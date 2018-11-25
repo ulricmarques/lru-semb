fe7de89e6254961ca3c525cfd6250ab15357d436
Ulric Marques <ulricmf@gmail.com>
2018-11-25 00:03:15 -0200

    Finalizando comentários

--- referencias.h
+++ referencias.h
@@ -1,11 +1,31 @@
 /* 
- * Arquivo:   lru.c
+ * Arquivo: referencias.h
+ *  
+ * Plataforma Alvo : PIC16F18875 (Microchip)
+ * Compilador : XC8 (Microchip)
+ * IDE :  MPLABX (Microchip)
  * Autores: Ulric Marques e Cláudia Mariana
  *
+ * Adaptado de www.programming9.com, com código original disponível em:
+ * <www.programming9.com/programs/c-programs/285-page-replacement-programs-in-c>.
+ * 
  * Criado em 14 de Novembro, 2018, 16:20
- *
+ * 
  *  Copyright (C) 2018 Cláudia Mariana <claudiamarianacmaia@gmail.com>
  *  Copyright (C) 2018 Ulric Marques  <ulricmf@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * Este arquivo armazena o vetor de referências escolhido para ser utilizado no
  * algoritmo LRU. Os elementos do vetor existente neste arquivo são utilizados 

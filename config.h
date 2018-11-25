4d7a9c64c5d4c501841ee08730560a8df052227b
Ulric Marques <ulricmf@gmail.com>
2018-11-24 23:59:55 -0200

    Finalizando comentários

---
+++ config.h
@@ -0,0 +1,79 @@
+/* 
+ * Arquivo: config.c
+ * 
+ * Plataforma Alvo : PIC16F18875 (Microchip)
+ * Compilador : XC8 (Microchip)
+ * IDE :  MPLABX (Microchip)
+ * Autores: Ulric Marques e Cláudia Mariana
+ *
+ * Adaptado de www.programming9.com, com código original disponível em:
+ * <www.programming9.com/programs/c-programs/285-page-replacement-programs-in-c>. 
+ * 
+ * Criado em 14 de Novembro, 2018, 16:20
+ * 
+ *  Copyright (C) 2018 Cláudia Mariana <claudiamarianacmaia@gmail.com>
+ *  Copyright (C) 2018 Ulric Marques  <ulricmf@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * 
+ * As configurações contidas neste arquivo foram geradas pela IDE ao setar
+ * alguns registradores.
+ *  Os registradores que não foram setados possuem valores padrão.
+*/
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#endif	/* CONFIG_H */
+
+#define _XTAL_FREQ 32000000
+#define F_CPU 32000000/64//#define Baud_value(baud_rate) (((float)(F_CPU)/(float)baud_rate)-1)
+#define Baud_value (((float)(F_CPU)/(float)baud_rate)-1)//cálculo da taxa de transmissão serial para UART
+
+// CONFIG1
+#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (Oscillator not enabled)
+#pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (FSCM timer enabled)
+
+// CONFIG2
+#pragma config MCLRE = ON       // Master Clear Enable bit (MCLR pin is Master Clear function)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config LPBOREN = OFF    // Low-Power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN = ON       // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
+#pragma config ZCD = OFF        // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
+#pragma config PPS1WAY = ON     // Peripheral Pin Select one-way control (The PPSLOCK bit can be cleared and set only once in software)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS_31// WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled, SWDTEN is ignored)
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4
+#pragma config WRT = OFF        // UserNVM self-write protection bits (Write protection off)
+#pragma config SCANE = available// Scanner Enable bit (Scanner module is available for use)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
+
+// CONFIG5
+#pragma config CP = OFF         // UserNVM Program memory code protection bit (Program Memory code protection disabled)
+#pragma config CPD = OFF        // DataNVM code protection bit (Data EEPROM code protection disabled)
+
+// CONFIG6
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+
